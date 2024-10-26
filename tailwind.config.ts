0866122a9e0357fc1b0fb0dddd9c5527ec292157
Poyotoron <20264082+Poyotoron@users.noreply.github.com>
2024-10-26 04:48:37 +0000

    ストライプ背景

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
+      backgroundImage: {
+        'stripe-pattern': 'repeating-linear-gradient(45deg, rgba(255, 255, 255, 0.2) 0, rgba(255, 255, 255, 0.2) 10px, rgba(238, 130, 238, 0.2) 10px, rgba(238, 130, 238, 0.2) 20px)',
+      }
     },
   },
   plugins: [],

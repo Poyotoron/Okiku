f322ed94d579ad6173cf18915b757b3f6edc3c52
Poyotoron <20264082+Poyotoron@users.noreply.github.com>
2024-10-26 10:52:59 +0000

    全体の構造は完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import { Rock_Salt } from "next/font/google"
 
 const rock_salt = Rock_Salt({
@@ -11,13 +11,46 @@ export default function Home() {
     <div className="bg-stripe-pattern justify-center items-center h-screen">
       <header className="flex flex-col">
         <div className={`${rock_salt.className} text-center`}>
-          <span style={{ fontSize: '100pt' }}>Okiku</span>
+          <span style={{ fontSize: '100pt', userSelect: 'none' }}>Okiku</span>
         </div>
       
         <div className="text-center">
           文字数/単語数カウントツール
         </div>
       </header>
+
+      <hr className="my-8 border-t-4 border-gray-300" />
+
+      <main className="flex flex-col">
+        <div>
+          <div className="text-center">
+            <h2>文字数カウント</h2>
+          </div>
+          工事中……
+        </div>
+
+        <hr className="my-8 border-t-2 border-gray-300" />
+
+        <div>
+          <div className="text-center">
+            <h2>単語数カウント</h2>
+          </div>
+          工事中……
+        </div>
+      </main>
+
+      <hr className="my-8 border-t-4 border-gray-300" />
+
+      <footer className="flex flex-col">
+        <div className="flex justify-center items-center mt-4 space-x-4">
+          <a href="https://twitter.com/PyTr_N" target="_blank" rel="noopener noreferrer">
+            <Image src="/logo_twitter_black.png" alt="Twitter Logo" width={32} height={32} />
+          </a>
+          <a href="https://github.com/Poyotoron/Okiku" target="_blank" rel="noopener noreferrer">
+            <Image src="/logo_github.svg" alt="GitHub Logo" width={32} height={32} />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }

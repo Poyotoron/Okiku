f65554e4deece228767da2ffaddd3e353474229c
Poyotoron <20264082+Poyotoron@users.noreply.github.com>
2024-11-12 05:33:51 +0000

    文字数カウントをちゃんと実装

--- src/app/counter.tsx
+++ src/app/counter.tsx
@@ -1,17 +1,18 @@
 "use server";
 
 type CharFormData = {
-    text: string;
-    includeSpace: boolean;
-  };
+  text: string;
+  includeSpace: boolean;
+};
 
 export async function countChar(formData: CharFormData) {
-    const isCountSpace = formData["includeSpace"];
-    // const text = formData["text-char"];
+  const isCountSpace = formData["includeSpace"];
+  const text = formData["text"];
 
-    if (isCountSpace) {
-        return 100;
-    } else {
-        return 200;
-    }
+  if (isCountSpace) {
+    return text.length;
+  } else {
+    const trimmedText = text.replace(/\s+/g, "");
+    return trimmedText.length === 0 ? 0 : trimmedText.length;
+  }
 }
